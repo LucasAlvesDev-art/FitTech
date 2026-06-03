f85801622c476b593733c5079ce465608318fa83
Lucas Alves <lucasalves0000003@gmail.com>
2026-06-03 11:59:07 -0300

    adicionei mais arquivos para desenvolver as novas telas, ainda incompleto a navegação do tabbottom

---
+++ src/pages/aluno/MeuTreino.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+
+
+
+export default function MeuTreino() {  
+    return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>
+                MEU TREINO
+            </Text>
+        </View>
+    );
+}
---
+++ src/pages/aluno/PerfilAluno.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+
+
+
+export default function PerfilAluno() {  
+    return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>
+                PERFIL DO ALUNO
+            </Text>
+        </View>
+    );
+}
