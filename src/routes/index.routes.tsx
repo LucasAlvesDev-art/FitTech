b93a506f01d4f4bdcfa504f50903a2c0c0b91b53
Lucas Alves <lucasalves0000003@gmail.com>
2026-06-10 14:54:27 -0300

    ajusta no app em geral, login e cadastro ok

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,35 +1,46 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { ActivityIndicator, View } from 'react-native';
 import Login from '../pages/login';
 import Cadastro from '../pages/cadastro';
+
 
 import BottomAlunos from './bottom.Alunos';
 import BottomInstrutor from './bottom.Instrutores';
 import { useAuth } from '../context/AuthContext';
+import { themas } from '../global/themes';
 
 export default function Routes() {
     const Stack = createStackNavigator();
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
 
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false,
-                cardStyle: {
-                    backgroundColor: '#fff'
-                }
-            }}
-        >
-            {!user ? (
-                <>
-                    <Stack.Screen name="Login" component={Login} />
-                    <Stack.Screen name="Cadastro" component={Cadastro} />
-                </>
-            ) : (
-                <>
-                    <Stack.Screen name="BottomAlunos" component={BottomAlunos} />
-                    <Stack.Screen name="BottomInstrutor" component={BottomInstrutor} />
-                </>
-            )}
-        </Stack.Navigator>
-    );
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator 
+                size="large" 
+                color={themas.colors.primary} 
+                />
+            </View>
+        );
+    }
+
+   return (
+  <Stack.Navigator 
+        screenOptions={{ 
+            headerShown: false 
+        }}>
+
+    {
+    !user ? (
+      <>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+      </>
+    ) : user.role === 'instrutor' ? (
+      <Stack.Screen name="BottomInstrutor" component={BottomInstrutor} />
+    ) : (
+      <Stack.Screen name="BottomAlunos" component={BottomAlunos} />
+    )}
+  </Stack.Navigator>
+);
 }
