a948defddee726b96626970bc1d4e7c3bb278875
Lucas Alves <lucasalves0000003@gmail.com>
2026-06-10 10:38:41 -0300

    Alterei o index.routes, instalei process, async storage, e atualizei o arquivo supabase

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,10 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
