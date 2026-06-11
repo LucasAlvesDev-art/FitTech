45bb82444f77c7476ebf7ccf04d9e21e1b4d534a
Lucas Alves <lucasalves0000003@gmail.com>
2026-06-10 23:04:42 -0300

    apaguei arquivos que não estavam sendo usados, coloque botão de logout apenas para testar navegação, logout ok

--- src/pages/aluno/ExercicioTreino.tsx
+++ src/pages/aluno/ExercicioTreino.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 import { Screen } from '../../components/Screen';
 import { themas } from '../../global/themes';
-
-
+import { useAuth } from '../../context/AuthContext';
 
 
 export default function Aluno() {  
+    const { logout } = useAuth();
+     const handleLogout = async () => {
+        await logout();
+    };
     return (    
         <Screen>
             <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+
+                <TouchableOpacity onPress={handleLogout}>
+                    <Text style={{ color: 'red', fontSize: 16 }}>
+                        Sair
+                    </Text>
+                </TouchableOpacity>
+                
                 <Text style={{ color: themas.colors.text }}>
                     EXERCICIO TREINO
                 </Text>
+                
             </View>
         </Screen>
     );
--- src/pages/instrutor/AdicionarExercicio.tsx
+++ src/pages/instrutor/AdicionarExercicio.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 import { Screen } from '../../components/Screen';
 import { themas } from '../../global/themes';
+import { useAuth } from '../../context/AuthContext';
 
 
 
 
 export default function Instrutor() {  
-    return (  
+
+    const { logout } = useAuth();
+
+    const handleLogout = async () => {
+        await logout();
+    };
+
+    return ( 
+         
         <Screen>  
             <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                
+                <TouchableOpacity onPress={handleLogout}>
+                    <Text style={{ color: 'red', fontSize: 16 }}>
+                        Sair
+                    </Text>
+                </TouchableOpacity>
+
                 <Text style={{ color: themas.colors.text }}>
                     ADICIONAR EXERCICIO
                 </Text>
+
             </View>
         </Screen>
     );
